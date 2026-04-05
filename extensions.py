3e56ad04d7679388411a5b9224512f55ba2491d0
Jonatanromao <jonatansilva3697@gmail.com>
2026-04-05 11:48:36 -0300

    Ajusta Render para usar threading no SocketIO

--- extensions.py
+++ extensions.py
@@ -1,6 +1,7 @@
 # extensions.py
+import os
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
 
 db = SQLAlchemy()
-socketio = SocketIO()
+socketio = SocketIO(async_mode=os.getenv("SOCKETIO_ASYNC_MODE", "threading"), cors_allowed_origins="*")
